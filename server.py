86a153396d6e5b19b2bd5a83d9fc06d04235669a
Vemula.Shanmukha Srinivas <36005710+ShanmukhaSrinivas@users.noreply.github.com>
2018-12-14 21:24:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-#Server side basic program
+#Server side Networking
 import socket
 
 host = '127.0.0.1'
-port = 10000 #Let it be b/w  10k to 50 k
+port = 10000 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -10,9 +10,8 @@ s.listen(1)
 c, addr = s.accept()
 print("The client got connected")
 
-c.send(b"Hello Client, are you the Velagala")
-c.send(b"Just go to Bhavana, you Mr.Peice of Shit")
-str = "Get Lost...!"
+c.send(b"Hello Client, are you doing well?")
+str = "Bye...!"
 c.send(str.encode())
 
 s.close()
