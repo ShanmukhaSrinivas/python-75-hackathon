6f51d7d19e68ad43787624621eb1697955aa0eab
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2018-12-15 02:02:18 +0530

    Indexing_Example

---
+++ indexing.py
@@ -0,0 +1,20 @@
+#indexing
+x='welcome to hackathon'
+
+#prints 3rd character from begining
+print(x[2])
+
+#prints 7th character from begining
+print(x[6])
+
+#prints 3rd character from end
+print(x[15])
+
+#prints the last character
+print(x[len(x)-1])
+
+#above line can be written as
+print(x[19])
+
+#whitespaces are also taken 
+print(x[7])
