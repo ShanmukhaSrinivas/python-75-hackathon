dc00272f8fe8e4b86f5058762b9b21e5ed180621
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2018-12-16 13:46:32 +0530

    Overloading

---
+++ overloading.py
@@ -0,0 +1,17 @@
+#Overloading
+
+def product(a, b): 
+    p = a * b 
+    print(p) 
+      
+# Second product method 
+ 
+def product(a, b, c): 
+    p = a * b*c 
+    print(p) 
+  
+# Uncommenting the below line shows an error     
+#product(4, 5) 
+  
+# This line will call the second product method 
+product(4, 5, 5) 
