b69571c4e4df8f970c103293735c44502b9eb25e
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2018-12-16 15:38:05 +0530

    for_loop

---
+++ For_loop.py
@@ -0,0 +1,20 @@
+#finding sum of elements of a list
+
+lst=[10,20,30]
+sum=0
+for i in lst:
+    sum=sum+i
+    print(sum)
+print('\n')
+
+#printing index values along with their names
+
+for i in range(1,11):
+    print(i,'CHENNAI')
+    print('\n')
+
+#dsplaying elements of a list using for
+
+lst=[10,20,30]
+for i in (lst):
+    print(i)
